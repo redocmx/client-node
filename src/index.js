cf2d19d106ad201049572def7aa8c95a40dabb90
David Osorio <david.osorio@therunningfox.com>
2024-07-02 12:06:57 -0500

    feat: implement config for redoc constructor

--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@ import Addenda from './addenda.js';
 import Service from './service.js';
 
 export default class Redoc {
-    constructor(apiKey) {
-        this.apiKey = apiKey || process.env.REDOC_API_KEY;
-        this.service = new Service(this.apiKey);
+    constructor(config) {
+
+        if (config && typeof config === 'string') {
+            this.apiKey = config;
+            this.service = new Service(this.apiKey);
+        }
+
+        if (config && typeof config === 'object') {
+            const { apiKey, apiUrl } = config
+
+            if (!apiKey) {
+                throw new Error(`You must provide the apiKey config.`);
+            }
+
+            this.service = new Service({ apiKey, apiUrl });
+        }
+
+        this.service = this.service || new Service();
     }
 
     get cfdi() {
--- src/service.js
+++ src/service.js
@@ -1,20 +1,31 @@
 export default class Service {
-    constructor(apiKey) {
-        this.apiKey = apiKey || process.env.REDOC_API_KEY;
-        this.apiUrl = process.env.REDOC_API_URL || 'https://api.redoc.mx/cfdis/convert';
+    constructor(config) {
+
+        if (config && typeof config === 'string') {
+            this.apiKey = config;
+        }
+
+        if (config && typeof config === 'object') {
+            this.apiKey = config?.apiKey;
+            this.apiUrl = config?.apiUrl;
+        }
+
+        this.apiKey = this.apiKey || process.env.REDOC_API_KEY;
+        this.apiUrl = this.apiUrl || process.env.REDOC_API_URL || 'https://api.redoc.mx/cfdis/convert';
+
     }
 
-    async cfdisConvert({file, payload}) {
+    async cfdisConvert({ file, payload }) {
 
         const formData = new FormData();
 
-        if(payload?.style_pdf){
+        if (payload?.style_pdf) {
             formData.append('style_pdf', payload.style_pdf);
         }
-        if(payload?.addenda){
+        if (payload?.addenda) {
             formData.append('addenda', payload.addenda);
         }
-        
+
         formData.append('xml', new Blob([file.content], { type: 'text/xml' }), { filename: 'document.xml' });
 
         try {
@@ -26,19 +37,19 @@ export default class Service {
                 },
                 body: formData,
             });
-    
+
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
-    
+
             const arrayBuffer = await response.arrayBuffer();
             const buffer = Buffer.from(arrayBuffer);
-    
+
             const headers = response.headers;
             const metadata = Buffer.from((headers.get('x-redoc-xml-metadata')), 'base64').toString('utf-8');
-            
+
             const transactionId = headers.get('x-redoc-transaction-id');
-            const totalPages = headers.get('x-redoc-pdf-total-pages'); 
+            const totalPages = headers.get('x-redoc-pdf-total-pages');
             const totalTime = headers.get('x-redoc-process-total-time');
 
             return { buffer, metadata, transactionId, totalPages, totalTime };
